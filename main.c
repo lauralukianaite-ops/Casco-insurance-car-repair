0b51a4358dc0bba3dd94bbd69b7fc5ea4a37503d
Laura <laura.lukianaite@gmail.com>
2026-04-10 17:15:37 +0300

    Implement dynamic config file loading and -rnd parameter handling

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include "BigInt.h"
 #include "pqueue.h"
 
@@ -15,16 +16,45 @@ typedef struct{
     int U; //darbuotojo atlyginimas
 }Params;
 
-int main(){
-    char file_name[50] = "data.txt";
-    Params p;
+int main(int argc, char *argv[]){
+    char file_name[256] = "";
+    int seed = -1;
 
+    //Ieskoma -rnd ir failo vardo
+    for(int i=1; i < argc; i++){
+        if(strcmp(argv[i], "-rnd") == 0 && i + 1 < argc){
+            seed = atoi(argv[i+1]);
+            i++;
+        } else if(file_name[0] == '\0'){ //ar failo vardas dar neivestas, jei ne rnd - tai bus config failas
+            strcpy(file_name, argv[i]);
+        }
+    }
+
+    if(file_name[0] == '\0'){
+        strcpy(file_name, argv[0]);
+        char *dot = strrchr(file_name, '.');
+        if(dot != NULL){
+            strcpy(dot, ".cfg");
+        } else { //jei galunes nebuvo
+            strcat(file_name, ".cfg");
+        }
+    }
+
+    if(seed == -1) {
+        seed = (int)time(NULL) % 1000;
+    }
+    srand(seed);
+    printf("Initialising seed: %d\n",seed);
+
+    Params p;
     FILE *f = fopen(file_name, "r");
     if(f==NULL){
         printf("Error! Cannot open file %s\n",file_name);
+        return 1;
     }
     if(fscanf(f, "%ld %d %d %d %d %d %d %d", &p.model_time, &p.K, &p.T1, &p.T2, &p.TR, &p.A, &p.A1, &p.U) != 8){
         printf("Error! Parameters missing or entered in incorrect format\n");
+        return 1;
     }
     fclose(f);
 
